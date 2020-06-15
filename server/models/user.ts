ef9c9ede0a886d24955274ab46e77af7ae01fd16
ttomz <ttomz4291@gmail.com>
2020-06-15 17:48:40 +0300

    update user mongoose schema

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,42 +1,54 @@
-import * as bcrypt from 'bcryptjs';
-import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcryptjs'
+import * as mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
   username: String,
   email: { type: String, unique: true, lowercase: true, trim: true },
   password: String,
-  role: String
-});
+  role: String,
+  pubgID: String,
+  pubgName: String,
+  facebookURL: String,
+  clan: String,
+})
 
 // Before saving the user, hash the password
-userSchema.pre('save', function(next) {
-  const user = this;
-  if (!user.isModified('password')) { return next(); }
+userSchema.pre('save', function (next) {
+  const user = this
+  if (!user.isModified('password')) {
+    return next()
+  }
   bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err); }
+    if (err) {
+      return next(err)
+    }
     bcrypt.hash(user.password, salt, (error, hash) => {
-      if (error) { return next(error); }
-      user.password = hash;
-      next();
-    });
-  });
-});
+      if (error) {
+        return next(error)
+      }
+      user.password = hash
+      next()
+    })
+  })
+})
 
-userSchema.methods.comparePassword = function(candidatePassword, callback) {
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) { return callback(err); }
-    callback(null, isMatch);
-  });
-};
+    if (err) {
+      return callback(err)
+    }
+    callback(null, isMatch)
+  })
+}
 
 // Omit the password when returning a user
 userSchema.set('toJSON', {
   transform: (doc, ret) => {
-    delete ret.password;
-    return ret;
-  }
-});
+    delete ret.password
+    return ret
+  },
+})
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
-export default User;
+export default User
