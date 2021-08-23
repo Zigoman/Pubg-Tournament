33c7875962a377c573aff4689317d1244557d125
Aviad Pineles <paviad2@gmail.com>
2021-08-23 22:41:42 +0300

    Emit loader events on the `asapScheduler` to avoid expression changed   after it has been checked errors.

--- src/app/shared/services/loader.service.ts
+++ src/app/shared/services/loader.service.ts
@@ -1,27 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, asapScheduler } from 'rxjs';
+import { debounceTime, distinctUntilChanged, observeOn } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderService {
+  private counterSubject: BehaviorSubject<number>;
+
   // private loadCounter: number;
   public loadCounter: number;
-  public counterValue: BehaviorSubject<number>;
+  public counterValue$: Observable<number>;
   // public handlerWithoutDelay: Subject<any> = new Subject();
 
   constructor() {
+    const LOADER_EVENTS_DEBOUNCE = 50;
+
     this.loadCounter = 0;
-    this.counterValue = new BehaviorSubject<number>(this.loadCounter);
+    this.counterSubject = new BehaviorSubject<number>(this.loadCounter);
+    this.counterValue$ = this.counterSubject.pipe(
+      debounceTime(LOADER_EVENTS_DEBOUNCE),
+      distinctUntilChanged(),
+      observeOn(asapScheduler)
+    );
   }
 
   public increaseCounter(): void {
     this.loadCounter += 1;
-    this.counterValue.next(this.loadCounter);
+    this.counterSubject.next(this.loadCounter);
   }
 
   public decreaseCounter(): void {
     this.loadCounter -= 1;
-    this.counterValue.next(this.loadCounter);
+    this.counterSubject.next(this.loadCounter);
   }
 }
